ca89a8dc7a7cf6fa899bfbdbb016bcee804be280
xiaozhi1995 <1264204146@qq.com>
2016-06-16 01:09:36 -0700

    rm

--- http_project/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-ROOT_PATH=$(pwd)
-conf=$ROOT_PATH/conf/server.conf
-ser==$ROOT_PATH/httpd
-
-ip=$(grep -E "^IP:" $conf | awk -F: '{print $NF}')
-port=$(grep -E "^PORT:" $conf | awk -F: '{print $NF}')
-$ser $ip $port
